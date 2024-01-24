bd77fb9d92d9cf73e6dd22600786147c47bca9ba
paul lestrat <paullestrat@hotmail.fr>
2024-01-24 23:06:13 +0100

    muscu!!!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,12 @@
-import DisplayInfo from './components/DisplayInfo'
 import './App.css'
+import InsultGenerator from './components/InsultGenerator'
 
 function App() {
 
   return (
     <>
-      <h1>Simple web app</h1>
-      <div className="card">
-        <DisplayInfo></DisplayInfo>
-      </div>
-      <p className="comment">
-        Paul Lestrat
-      </p>
+      <h1>Gym motivation</h1>
+      <InsultGenerator/>
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -5,9 +5,29 @@ app.use(cors())
 
 const port = 3500;
 
+const insultList = [
+    "YOU SON OF A BITCH",
+    "YOU'RE AS USELESS AS THE POPE'S BALLS",
+    "MY GRANDMA LIFTS HEAVIER THAN YOU",
+    "YOUR PARENTS WOULD BE ASHAMED OF YOU",
+    "AN ANNOREXIC MIDGET WOULD BE MORE SEXY",
+    "YOUR DICK IS BELOW AVERAGE",
+    "NOONE LOVES YOU",
+    "WHO CARES ABOUT THE STUFF YOU DO",
+    "YOU'VE DONE NOTHING OF YOUR DAY AGAIN",
+    "FUCK YOU",
+    "STOP FUCKING AROUND AND GET TO WORK",
+    "WHO THE FUCK ARE YOU",
+]
+
 app.get("/items", (req, res) => {
-    console.log("Received GET request at /items endpoint")
+    console.log("Received GET request at /items endpoint");
     res.send({items: ["This comes from the server", "incredible", "I am absolutely loving this"]})
+})
+
+app.get("/insult", (req, res) => {
+    console.log("Received GET request at /insult endpoint");
+    res.send({insult: insultList[Math.floor(Math.random()*insultList.length)]})
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
